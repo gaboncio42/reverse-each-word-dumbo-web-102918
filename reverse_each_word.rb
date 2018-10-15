dd7cbbbfd26322644af21662b1c9d5ed8b5aa7c2
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-15 22:20:27 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
-  reversed_array = sentence.split
-  reversed_array.collect.join do |word|
+  reversed_array = sentence.split(" ")
+  reversed_array.collect do |word|
     word.reverse
   end
+  reversed_array.join(" ")
 end
